2b152112419d9f21712c3bee98aaccf4b3f75dcc
adiyous <adilnewyork@gmail.com>
2020-03-13 10:31:43 -0800

    Moving booklist array from App.js to Library.js and maping to display App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const bookList = [
-  {title: "Don Quixote", author: "Miguel de Cervantes"},
-  {title: "The Grat Gatsby", author: "F. Scott Fitzgerald"},
-  {title: "War and Peace", author: "Leo Tolstoy"},
-]
+import Library from './Library';
 
 function App() {
   return (
@@ -14,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Books app with React</h1>
+        <Library />
       </header>
     </div>
   );
--- src/Library.js
+++ src/Library.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react"
 
+const bookList = [
+    {title: "Don Quixote", author: "Miguel de Cervantes"},
+    {title: "The Grat Gatsby", author: "F. Scott Fitzgerald"},
+    {title: "War and Peace", author: "Leo Tolstoy"},
+  ]
+  
+
 class Library extends Component {
     render(){
+        const books = bookList.map((book) => <li>{book.title}</li>)
         return (
             <div>
-
+                Welcome to Book List
+                <ul>{books}</ul>
             </div>
         )
     }
