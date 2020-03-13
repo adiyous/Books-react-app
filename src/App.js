44219358224777208f3b125f3f729d63d687bbaf
adiyous <adilnewyork@gmail.com>
2020-03-13 09:43:34 -0800

    Adding Book lists array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const bookList = [
+  {title: "Don Quixote", author: "Miguel de Cervantes"},
+  {title: "The Grat Gatsby", author: "F. Scott Fitzgerald"},
+  {title: "War and Peace", author: "Leo Tolstoy"},
+]
+
 function App() {
   return (
     <div className="App">
