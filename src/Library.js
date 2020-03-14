1419755b8897fbf245e43c7452610eddc8bab049
adiyous <adilnewyork@gmail.com>
2020-03-14 13:58:08 -0800

    Revert back to react bootstrap

--- src/Library.js
+++ src/Library.js
@@ -13,25 +13,30 @@ class Library extends Component {
     render(){
         const books = bookList.map((book) => <li>{book.title}</li>)
         return (
-            <div>
-                <div>
-                    Welcome to Book List
-                    <ul>{books}</ul>
-                </div>
-                <div className="search-books">
-                    <div>
-                        <Button variant="primary" onClick={() => this.setState({ showSearchPage: false })}>
-                            Close
-                        </Button>
-                        <InputGroup className="mb-3">
-                            <InputGroup.Prepend>
-                            <Button variant="outline-secondary">Close</Button>
-                            </InputGroup.Prepend>
-                            <FormControl aria-describedby="basic-addon1" placeholder="Search by title or author"/>
-                        </InputGroup>
-                    </div>
-                </div>
+            <div className="search-books">
+            <div className="search-books-bar">
+              <button 
+                className="close-search" 
+                onClick={() => this.setState({ showSearchPage: false })}
+              >
+                    Close
+              </button>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author"/>
+
+              </div>
             </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
         )
     }
 }
